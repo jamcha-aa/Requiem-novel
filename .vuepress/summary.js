b0419ebb88bdcca0a4084ccf9ded4548ae518d19
jamcha <jamcha.aa@gmail.com>
2019-02-23 05:18:03 +0900

    Mod summary

--- .vuepress/summary.js
+++ .vuepress/summary.js
@@ -2,7 +2,7 @@ exports.genSidebarConfig = function () {
     return [
             {
                 title: '良き日々の終わり',
-                children: ['001']
+                children: ['', '001', '002', '003']
             }
     ]
 }
